27672840a5a2b89eff8988ebd5f0b19f586b0a61
Leandro <rissov.leandro@gmail.com>
2021-04-06 23:25:29 -0300

    Scraping the conjugation of future and present

--- anki-conjugator.py
+++ anki-conjugator.py
@@ -1,6 +1,7 @@
 import os
 from selenium.webdriver import Firefox
 import time
+from bs4 import BeautifulSoup
 
 #Create display variable
 os.environ["DISPLAY"] = ":0"
@@ -8,17 +9,28 @@ os.environ["DISPLAY"] = ":0"
 driver = Firefox()
 driver.get("https://conjugator.reverso.net/conjugation-french.html")
 
-search_box = driver.find_element_by_name('ctl00$txtVerb')
-search_box.send_keys('Faire')
+words = ['faire', 'aller']
 
-conjugate = driver.find_element_by_id('lbConjugate')
-conjugate.click()
+tic = time.time()
 
-search_box = driver.find_element_by_name('ctl00$txtVerb')
-search_box.send_keys('Aller')
+for word in words:
+    #Loop through desired words
+    search_box = driver.find_element_by_name('ctl00$txtVerb')
+    search_box.send_keys(word)
 
-conjugate = driver.find_element_by_id('lbConjugate')
-conjugate.click()
+    conjugate = driver.find_element_by_id('lbConjugate')
+    conjugate.click()
+    
+    present = driver.find_element_by_xpath('/html/body/div[2]/div/div/div[1]/div/form/div[3]/div/div[1]/div[4]/div/div/div[1]/div[2]/div').text
+    print(present)
 
-time.sleep(5)
+    print('-------')
+
+    future = driver.find_element_by_xpath('/html/body/div[2]/div/div/div[1]/div/form/div[3]/div/div[1]/div[4]/div/div/div[1]/div[4]/div').text
+    print(future)
+
+tac = time.time()
+
+print(tac - tic, ' segundos')
+
 driver.quit()
