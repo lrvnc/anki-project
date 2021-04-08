c9064e0f719be9a83c9eb39699381bfa6d646b28
Leandro <rissov.leandro@gmail.com>
2021-04-07 23:07:05 -0300

    Working passe compose, present, futur

--- anki-conjugator.py
+++ anki-conjugator.py
@@ -10,7 +10,7 @@ driver = Firefox()
 driver.get("https://conjugator.reverso.net/conjugation-french.html")
 
 #List of desired words
-words = ['sortir', 'mettre', 'prendre']
+words = ['sortir', 'mettre', 'prendre', 'faire']
 
 #Lists to save the verbs
 card_frontside = []
@@ -29,9 +29,10 @@ for word in words:
     #Scrap le présent, le futur and le passé composé
     present = driver.find_element_by_xpath('/html/body/div[2]/div/div/div[1]/div/form/div[3]/div/div[1]/div[4]/div/div/div[1]/div[2]/div').text.split()
     futur = driver.find_element_by_xpath('/html/body/div[2]/div/div/div[1]/div/form/div[3]/div/div[1]/div[4]/div/div/div[1]/div[4]/div').text.split()
-    #! passe_compose = driver.find_element_by_xpath('/html/body/div[2]/div/div/div[1]/div/form/div[3]/div/div[1]/div[4]/div/div/div[3]/div[2]/div').text.split()
+    passe_compose = driver.find_element_by_xpath('/html/body/div[2]/div/div/div[1]/div/form/div[3]/div/div[1]/div[4]/div/div/div[3]/div[2]/div').text.split()
+    print(passe_compose)
 
-    #Parsing le présent
+    #Formating le présent
     card_frontside.append(present[0] + ': ' + word) #Setting the front of the card
 
     for i in range(1,len(present),2): #Setting the back of the card
@@ -40,7 +41,7 @@ for word in words:
     card_backside.append(back[0:len(back)-2])
     back = ''
 
-    #Parsing le futur
+    #Formating le futur
     card_frontside.append(futur[0] + ': ' + word) #Setting the front of the card
 
     for i in range(1,len(futur),2): #Setting the back of the card
@@ -48,6 +49,26 @@ for word in words:
 
     card_backside.append(back[0:len(back)-2])
     back = ''
+
+    #Formating le passé composé
+    card_frontside.append(passe_compose[0] + ' ' + passe_compose[1] + ' (avec avoir): ' + word)
+    
+    back = passe_compose[2].capitalize() + ' ' + passe_compose[3] + ', '
+    for i in range(4,19,3): #Setting the back of the card
+        back += passe_compose[i].capitalize() + ' ' + passe_compose[i+1] + ' ' + passe_compose[i+2] + ', '
+    card_backside.append(back[0:len(back)-2])
+    back = ''
+
+    if len(passe_compose) != 19:
+        card_frontside.append(passe_compose[0] + ' ' + passe_compose[1] + ' (avec être): ' + word)
+        
+        for i in range(19,len(passe_compose),3): #Setting the back of the card
+            back += passe_compose[i].capitalize() + ' ' + passe_compose[i+1] + ' ' + passe_compose[i+2] + ', '
+            print(back)
+        card_backside.append(back[0:len(back)-2])
+        back = ''
+
+
 
 d = {'front': card_frontside, 'back': card_backside}
 df = pd.DataFrame(data=d)
